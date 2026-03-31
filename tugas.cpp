8df6fa7fc54b8574ff8b74efae2179df138f2dfe
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-31 21:45:06 +0700

    while loop

--- tugas.cpp
+++ tugas.cpp
@@ -4,3 +4,11 @@ using namespace std;
 // Variabel Global
 int angkaInput;
 int pilihan;
+
+// Function bool untuk mengecek bilangan prima menggunakan while loop
+bool cekPrima(int n)
+{
+    if (n <= 1)
+        return false;
+    int i = 2;
+    while (i * i <= n)
