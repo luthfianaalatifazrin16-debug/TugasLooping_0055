90030bbe76d082aa97a442beb4d541efef5261be
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-31 21:43:15 +0700

    int pilihan

--- tugas.cpp
+++ tugas.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// varable global
+// Variabel Global
 int angkaInput;
 int pilihan;
-
-// Function bool untuk mengecek bilangan prima menggunakan while loop
-bool cekPrima(int n)
-{
-    if (n <= 1)
-        return false;
-    int i = 2;
-    while (i * i <= n)
-    {
-        if (n % i == 0)
-            return false;
-        i++;
-    }
-    return true;
-}
-
-// Function bool untuk mengecek bilangan fibonacci menggunakan while loop
-bool cekFibonacci(int n)
-{
-}
