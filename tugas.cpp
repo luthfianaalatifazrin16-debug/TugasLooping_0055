1d0a7c0467600b27fee678ccef79e36ac557361a
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-31 21:34:32 +0700

    cekprima

--- tugas.cpp
+++ tugas.cpp
@@ -3,4 +3,19 @@ using namespace std;
 
 // varable global
 int angkaInput;
-int pilihsn;
+int pilihan;
+
+// Function bool untuk mengecek bilangan prima menggunakan while loop
+bool cekPrima(int n)
+{
+    if (n <= 1)
+        return false;
+    int i = 2;
+    while (i * i <= n)
+    {
+        if (n % i == 0)
+            return false;
+        i++;
+    }
+    return true;
+}
